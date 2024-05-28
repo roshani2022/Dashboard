efaf53048a3226ced23302581c7bf62bb7573d2f
roshani2022 <rosh.gupta17@gmail.com>
2024-05-28 17:58:54 +0530

    Dashboard

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import  Leaderboard from './components/Dashboard/Leaderboard';
+import Header from "./components/navigation/Header";
+function App() {
+ 
+
+  return (
+    <>
+      <Header />
+      <Leaderboard/>
+    </>
+
+  )
+}
+
+export default App
---
+++ src/components/layout/Root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../../navigation/Header';
+function Root() {
+  return (
+    <div>
+     <Header />
+    </div>
+  )
+}
+
+export default Root
